ca6e0a75c8a69b3c6a73a7c491abd80b48fe59f4
Nicolas Porter <porter.nicolas@gmail.com>
2020-03-08 16:16:19 -0700

    Add nano config (w/ syntax submodule)

--- zsh/.zshrc.d/00-stow.sh
+++ zsh/.zshrc.d/00-stow.sh
@@ -1,6 +1,10 @@
 # Stuff that will be re-stowed
 STOWED=(
-    zsh vscode spectacle neofetch
+    zsh
+    vscode
+    spectacle
+    neofetch
+    nano
 )
 export STOW_DIR="$DOTFILES"
 for pkg in "${STOWED[@]}"; do stow -R "$pkg"; done
