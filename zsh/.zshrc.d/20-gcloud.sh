c9cfa791e9015687ccb4d0bad93e5550484929d3
Nicolas Porter <porter.nicolas@gmail.com>
2020-07-25 17:45:16 -0700

    zsh: Fix for gcloud python interpreter

--- zsh/.zshrc.d/20-gcloud.sh
+++ zsh/.zshrc.d/20-gcloud.sh
@@ -4,3 +4,5 @@ export GCLOUD_HOME=/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
 
 source "$GCLOUD_HOME/path.zsh.inc"
 source "$GCLOUD_HOME/completion.zsh.inc"
+
+export CLOUDSDK_GSUTIL_PYTHON=${CLOUDSDK_GSUTIL_PYTHON:-CLOUDSDK_PYTHON}
