3bdf28f4bc9667b43dad2fe3ff21a8bc02c62af2
Nicolas Porter <porter.nicolas@gmail.com>
2020-12-07 12:09:01 -0800

    zsh: add support for a Stowfile in 00-stow.sh, some linting

--- zsh/.zshrc.d/00-stow.sh
+++ zsh/.zshrc.d/00-stow.sh
@@ -1,25 +1,33 @@
-# Stuff that you want to stow / re-stow on login
-STOWED=(
-    zsh
-    git
-    vscode
-    spectacle
-    neofetch
-    nano
-)
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+
+export STOW_DIR="$DOTFILES"
+
+# This is where the list of dirs to stow are specified
+export STOWFILE="${STOWFILE:-$STOW_DIR/Stowfile}"
 
 # Remove macOS turds, they can cause issues
-for f in `find "$DOTFILES" -name ".DS_Store"`; do
-    echo ${fg[yellow]}removing turd:${fg[default]} $f;
-    rm "$f"
-done
+# for f in **/.DS_STORE; do
+#     # shellcheck disable=SC2154
+#     echo "${fg[yellow]}removing turd:${fg[default]} $f";
+#     rm "$f"
+# done 2> /dev/null
 
-# Gitk just replaces the symlink with a file, so we fix that here
-gitk="$HOME/.config/git/gitk"
-if [ ! -L "$gitk" ]; then
-    mv "$gitk" "$DOTFILES/git/.config/git/gitk" || true
-fi
 
 # Restow
-export STOW_DIR="$DOTFILES"
-for pkg in "${STOWED[@]}"; do stow -R "$pkg"; done
+if [[ -f "$STOWFILE" ]]; then
+    while read -r package; do
+        [[ -z "$package" ]] && continue
+
+        # Gitk just replaces the symlink with a file, we fix that here before restow
+        if [[ "$package" == git ]]; then
+            gitk="$HOME/.config/git/gitk"
+            if [ ! -L "$gitk" ]; then
+                mv "$gitk" "$DOTFILES/git/.config/git/gitk" || true
+            fi
+        fi
+
+        stow -R "$package"
+
+    done < "$STOWFILE"
+fi
