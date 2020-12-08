fbbb147d94e20cdbd5ae8f50dcaf72b2614857fa
Nicolas Porter <porter.nicolas@gmail.com>
2020-12-08 09:11:02 -0800

    zsh: add 'sshconf' command

--- zsh/.zshrc.d/10-ssh.sh
+++ zsh/.zshrc.d/10-ssh.sh
@@ -16,4 +16,58 @@ function ssh_tunnel() {
   ssh -N -T -L "0.0.0.0:${src}:localhost:${dst}" "${host}"
 }
 
-alias sshconf="nano $HOME/.ssh/config"
+
+_SSHCONF_SEARCH_PATH="$HOME/.ssh/config.d"
+
+function _sshconf__list_files {
+    local query="$1"
+    find "${_SSHCONF_SEARCH_PATH}" -name "*${query}*" -type f
+}
+
+function _edit {
+    local file="$1"
+    if is_vscode_terminal; then
+        code -w "$file"
+    else
+        $VISUAL $file
+    fi
+}
+
+# Usage: sshconf [query]
+function sshconf {
+    local query="$1"
+
+    # No arguments: open dotfiles in editor
+    if [[ -z "$query" ]]; then
+        $VISUAL "$_SSHCONF_SEARCH_PATH"
+
+    # sshconf [query]: tries to match the query against an existing file
+    else
+        result=$(_sshconf__list_files "$query" | head -n1)
+
+        # query found...
+        # shellcheck disable=SC1046,SC1073
+        if [[ -f "$result" ]]; then
+            [[ ! -t 1 ]] && echo "$result" && return 0
+            >&2 echo "${fg[yellow]}edited:${fg[default]} $result"
+            _edit "$result"
+        # query not found...
+        else
+            [[ -t 1 ]] && >&2 {
+                echo "${fg[red]}error:${fg[default]} could not find ${fg[cyan]}${query}${fg[default]}"
+            }
+            return 1
+        fi
+    fi
+
+    return 0
+}
+
+# Tab completion for 'sshconf' command
+compdef __sshconf_completions sshconf
+function __sshconf_completions {
+    local _slugs="$(_sshconf__list_files | xargs -L 1 basename)"
+    _arguments -C \
+        "1: :($_slugs)" \
+        "*::arg:->args"
+}
