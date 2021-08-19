734c8f028adf0f0d7a81a9441916f097032f9178
Nicolas Porter <porter.nicolas@gmail.com>
2021-08-19 12:21:08 +0200

    refactor: consolidate shell theme / plugins

---
+++ zsh/.zshrc.d/03-plugins.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+# shellcheck disable=SC1090
+
+# Disable oh my zsh update check if running in vscode terminal
+if [[ "$TERM_PROGRAM" == vscode ]]; then
+    export DISABLE_AUTO_UPDATE=1
+fi
+
+if not is_installed antibody; then
+    # shellcheck disable=SC2154
+    warn "${fg[cyan]}antibody${fg[default]} not installed"
+    return
+fi
+source <(antibody init)
+
+# powerlevel10k
+PL10K_INSTANT_PROMPT_SCRIPT="${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+[[ -r "$PL10K_INSTANT_PROMPT_SCRIPT" ]] && source "$PL10K_INSTANT_PROMPT_SCRIPT"
+antibody bundle romkatv/powerlevel10k
+
+# oh-my-zsh
+ZSH="$(antibody home)/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh"
+export ZSH
+antibody bundle ohmyzsh/ohmyzsh
+
+# history / fzf
+is_installed fzf && antibody bundle ohmyzsh/ohmyzsh path:plugins/fzf
+
+# misc
+antibody bundle TamCore/autoupdate-oh-my-zsh-plugins
+antibody bundle zsh-users/zsh-syntax-highlighting
+antibody bundle ohmyzsh/ohmyzsh path:plugins/command-not-found
+antibody bundle ohmyzsh/ohmyzsh path:plugins/github
+antibody bundle ohmyzsh/ohmyzsh path:plugins/httpie
+antibody bundle ohmyzsh/ohmyzsh path:plugins/brew
+
+# devops
+antibody bundle ohmyzsh/ohmyzsh path:plugins/kubectl
+antibody bundle ohmyzsh/ohmyzsh path:plugins/aws
+antibody bundle ohmyzsh/ohmyzsh path:plugins/gcloud
+antibody bundle ohmyzsh/ohmyzsh path:plugins/docker
+antibody bundle jsporna/terraform-zsh-plugin
+# antibody bundle robbyrussell/oh-my-zsh path:plugins/terraform
+
+# node
+antibody bundle ohmyzsh/ohmyzsh path:plugins/node
+antibody bundle ohmyzsh/ohmyzsh path:plugins/yarn
+antibody bundle lukechilds/zsh-better-npm-completion
+antibody bundle favware/zsh-lerna
+
+antibody bundle cowboyd/zsh-volta
+
+export NVM_COMPLETION=true
+export NVM_AUTO_USE=false
+export NVM_LAZY_LOAD=true
+export NVM_LAZY_LOAD_EXTRA_COMMANDS=(lerna coffee ts-node tsc)
+export NVM_NO_USE=true
+antibody bundle lukechilds/zsh-nvm
+
+# python
+antibody bundle "MichaelAquilina/zsh-autoswitch-virtualenv"
+
+# AUTOSWITCH_MESSAGE_FORMAT="$(tput setaf 1)Switching to %venv_name 🐍 %py_version $(tput sgr0)"
+AUTOSWITCH_MESSAGE_FORMAT=""
+export AUTOSWITCH_MESSAGE_FORMAT
+antibody bundle ohmyzsh/ohmyzsh path:plugins/pyenv
+# antibody bundle ohmyzsh/ohmyzsh path:plugins/pipenv
+
+# ruby
+antibody bundle ohmyzsh/ohmyzsh path:plugins/ruby
+antibody bundle ohmyzsh/ohmyzsh path:plugins/rbenv
+
+# ssh-agent
+(   zstyle :omz:plugins:ssh-agent agent-forwarding on
+    antibody bundle ohmyzsh/ohmyzsh path:plugins/ssh-agent
+    eval "$(ssh-agent -s)"
+) &>/dev/null
+
+# gpg-agent
+# antibody bundle robbyrussell/oh-my-zsh path:plugins/gpg-agent
---
+++ zsh/.zshrc.d/04-theme.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+
+
+# Some of these taken here...
+# https://coderwall.com/p/pn8f0g/show-your-git-status-and-branch-in-color-at-the-command-prompt
+
+is_git_repo() {
+  git diff --quiet --ignore-submodules HEAD >/dev/null 2>&1
+}
+
+git_stats() {
+  if is_git_repo; then
+    setopt local_options BASH_REMATCH
+    local total_insertions=0
+    local total_deletions=0
+
+    # shellcheck disable=SC2030
+    while read -r value; do
+      if [[ ${value} =~ ([[:digit:]]+)\ insertions.*\ ([[:digit:]]+)\ deletions ]]; then
+        ((total_insertions = total_insertions + ${BASH_REMATCH[2]:-0}))
+        ((total_deletions = total_deletions + ${BASH_REMATCH[3]:-0}))
+      fi
+    done < <(
+      git diff --shortstat --cached
+      git diff --shortstat
+    )
+
+    # shellcheck disable=SC2031
+    [[ $total_insertions -eq 0 ]] && total_insertions="" || total_insertions="%F{yellow}+$total_insertions%F{clear}"
+
+    # shellcheck disable=SC2031
+    [[ $total_deletions -eq 0 ]] && total_deletions="" || total_deletions="%F{red}-$total_deletions%F{clear}"
+
+    echo "$total_insertions $total_deletions"
+  fi
+}
+
+custom_git_branch() {
+  local branch
+  branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+  if git diff-index --quiet HEAD >/dev/null 2>&1; then
+    echo -n "%F{green}"
+  else
+    echo -n "%F{red}"
+  fi
+  echo "$branch%F{clear}"
+}
+
+POWERLEVEL9K_CUSTOM_GIT_BRANCH="custom_git_branch"
+POWERLEVEL9K_CUSTOM_GIT_BRANCH_ICON=""
+POWERLEVEL9K_CUSTOM_GIT_BRANCH_SEGMENT_ICON=""
+POWERLEVEL9K_CUSTOM_GIT_BRANCH_BACKGROUND="clear"
+POWERLEVEL9K_CUSTOM_GIT_BRANCH_FOREGROUND="008"
+
+
+custom_pyenv() {
+    local version;
+    version=$(pyenv version-alias)
+    if [[ "${version}" != "global" ]]; then
+        echo "${version//\%/%%}"
+    fi
+}
+POWERLEVEL9K_CUSTOM_PYENV="custom_pyenv"
+POWERLEVEL9K_CUSTOM_PYENV_BACKGROUND='clear'
+export VIRTUAL_ENV_DISABLE_PROMPT=
+
+
+# custom_git_chain() {
+#    git chain list --short | grep '^\*'
+# }
+# if is_installed git-chain; then
+#   POWERLEVEL9K_CUSTOM_GIT_CHAIN="custom_git_chain"
+#   POWERLEVEL9K_CUSTOM_GIT_CHAIN_BACKGROUND="clear"
+#   POWERLEVEL9K_CUSTOM_GIT_CHAIN_FOREGROUND="008"
+#   POWERLEVEL9K_CUSTOM_GIT_CHAIN_ICON=""
+#   POWERLEVEL9K_CUSTOM_GIT_CHAIN_SEGMENT_ICON=""
+# fi
+
+custom_time() {
+  local now
+  now="$(date +%H:%M:%S)"
+  echo -e "%F{008}$now\n%F{237}│%F{clear}"
+}
+POWERLEVEL9K_CUSTOM_TIME="custom_time"
+POWERLEVEL9K_CUSTOM_TIME_BACKGROUND="clear"
+POWERLEVEL9K_CUSTOM_TIME_FOREGROUND="008"
+POWERLEVEL9K_CUSTOM_TIME_ICON=""
+POWERLEVEL9K_CUSTOM_TIME_SEGMENT_ICON=""
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+  custom_time
+  dir
+  virtualenv_joined
+  # custom_pyenv_joined
+  custom_git_branch_joined
+)
+
+# ╭─
+# ╰─
+# ┌─
+# └─
+# ┏━
+# ┗━
+# ╔═
+# ╚═
+# ▛▀
+# ▙▄
+
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%F{237}╭%F{clear}"
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{237}╰%F{clear} "
+
+typeset -g POWERLEVEL9K_{LEFT,RIGHT}_SUBSEGMENT_SEPARATOR=''
+
+POWERLEVEL9K_DISABLE_RPROMPT=true
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+
+icons=()
+POWERLEVEL9K_MODE='nerdfont-complete'
+
+typeset -g POWERLEVEL9K_VCS_{GIT,GITHUB,BITBUCKET}_ICON=""
+POWERLEVEL9K_HIDE_BRANCH_ICON=true
+POWERLEVEL9K_VCS_STAGED_ICON="\b"
+POWERLEVEL9K_VCS_HIDE_TAGS=true
+POWERLEVEL9K_VCS_SHOW_SUBMODULE_DIRTY=false
+POWERLEVEL9K_VCS_UNSTAGED_ICON="\b"
+POWERLEVEL9K_VCS_GIT_HOOKS=()
+POWERLEVEL9K_SHOW_CHANGESET=false
+
+POWERLEVEL9K_VIRTUALENV_PYTHON_ICON=
+POWERLEVEL9K_VIRTUALENV_BACKGROUND='clear'
+POWERLEVEL9K_VIRTUALENV_FOREGROUND='008'
+
+POWERLEVEL9K_HOST_FOREGROUND="grey"
+POWERLEVEL9K_HOST_BACKGROUND="clear"
+POWERLEVEL9K_HOST_ICON=""
+POWERLEVEL9K_USER_ICON=""
+
+POWERLEVEL9K_USER_BACKGROUND="clear"
+
+POWERLEVEL9K_DIR_BACKGROUND='237'
+POWERLEVEL9K_DIR_DEFAULT_BACKGROUND="clear"
+POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="008"
+POWERLEVEL9K_DIR_FOREGROUND='010'
+POWERLEVEL9K_DIR_HOME_BACKGROUND="clear"
+POWERLEVEL9K_DIR_HOME_FOREGROUND="012"
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND="clear"
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="012"
+
+POWERLEVEL9K_DIR_PATH_SEPARATOR="%F{008}/%F{cyan}"
+
+POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=''
+POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR=''
+
+POWERLEVEL9K_HOME_ICON=""
+POWERLEVEL9K_HOME_SUB_ICON=""
+POWERLEVEL9K_DIR_ETC_BACKGROUND="clear"
+POWERLEVEL9K_ETC_ICON=""
+
+POWERLEVEL9K_DIR_WRITABLE_FORBIDDEN_FOREGROUND="red"
+POWERLEVEL9K_DIR_WRITABLE_FORBIDDEN_BACKGROUND="clear"
+
+POWERLEVEL9K_GO_ICON="\uf7b7"
+POWERLEVEL9K_GO_VERSION_BACKGROUND='clear'
+POWERLEVEL9K_GO_VERSION_FOREGROUND='081'
+
+POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%F{clear}\u0008'
+# POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%F{008}\u2e2b%F{008}'
+# POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR="%F{237}〉%F{clear}"
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+
+POWERLEVEL9K_NVM_BACKGROUND='clear'
+POWERLEVEL9K_NVM_FOREGROUND='green'
+
+POWERLEVEL9K_OS_ICON_BACKGROUND='clear'
+POWERLEVEL9K_OS_ICON_FOREGROUND='cyan'
+
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+
+
+typeset -g POWERLEVEL9K_STATUS_{,OK_,ERROR_}BACKGROUND="clear"
+POWERLEVEL9K_STATUS_ERROR_FOREGROUND="001"
+POWERLEVEL9K_CARRIAGE_RETURN_ICON="\uf071"
+
+POWERLEVEL9K_TIME_FORMAT="%D{%H:%M \uE868  %d.%m}"
+
+typeset -g POWERLEVEL9K_VCS_{CLEAN,MODIFIED,UNTRACKED}_BACKGROUND='clear'
+typeset -g POWERLEVEL9K_VCS_{CLEAN,UNTRACKED}_FOREGROUND='green'
+POWERLEVEL9K_VCS_MODIFIED_FOREGROUND='yellow'
