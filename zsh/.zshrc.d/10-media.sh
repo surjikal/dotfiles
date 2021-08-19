42af76fd920aef6e1e63cd748cd2cb1a5eeb550c
Nicolas Porter <porter.nicolas@gmail.com>
2021-08-19 12:12:36 +0200

    lint 10-media.sh, some tweaks to youtube dl stuff

--- zsh/.zshrc.d/10-media.sh
+++ zsh/.zshrc.d/10-media.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env zsh
 # shellcheck shell=bash
 
-
 # Downloads audio from a youtube video
 function youtube-audio {
-    youtube-dl --extract-audio --audio-format flac --prefer-ffmpeg $1
+    youtube-dl --extract-audio --audio-format flac --prefer-ffmpeg "$1"
 }
 
 function youtube-video {
-    youtube-dl --extract-video --prefer-ffmpeg $1
+    youtube-dl --format='bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best' --extract-video --prefer-ffmpeg "$1"
 }
 
 # Extracts audio from a video
 function video-audio-extract {
-    ffmpeg -i $1 -vn -acodec libvorbis $1-audio.ogg
+    ffmpeg -i "$1" -vn -acodec libvorbis "$1-audio.ogg"
 }
 
 # Removes audio from a video
 function video-audio-strip {
-    ffmpeg -i $1 -c copy -an "$1-nosound.${1#*.}"
+    ffmpeg -i "$1" -c copy -an "$1-nosound.${1#*.}"
 }
 
 # Takes whatever video format and optimizes it
@@ -27,25 +26,26 @@ function video-normalize {
     local inputfile="$1"
     local outputfile="${inputfile%.*}.mp4"
     ffmpeg -i "$inputfile" -c:v libx264 -preset veryfast "$outputfile"
-    echo "Wrote: $ouputfile"
-    ls -lah $inputfile
-    ls -lah $outputfile
+    echo "Wrote: $outputfile"
+    ls -lah "$inputfile"
+    ls -lah "$outputfile"
 }
 
-
 function image-normalize {
-    for filename; do (
-      local output="$(file_name "$filename").jpg"
-      echo "$filename -> $output"
-      magick convert "$filename" "$output"
-      magick mogrify -resize 1000x1000 "$output"
-    ) &
+    for filename; do
+        (
+            local output
+            output="$(file_name "$filename").jpg"
+            echo "$filename -> $output"
+            magick convert "$filename" "$output"
+            magick mogrify -resize 1000x1000 "$output"
+        ) &
     done
     wait
 }
 
-is_installed zbarimg && alias qr-decode="zbarimg"
-
 function ocr {
-    echo $(convert $1 -resize 400% -type Grayscale - | tesseract - -)
+    convert "$1" -resize 400% -type Grayscale - | tesseract - -
 }
+
+is_installed zbarimg && alias qr-decode="zbarimg"
