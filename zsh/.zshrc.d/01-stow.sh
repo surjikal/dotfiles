aab701af6054f75732a1fca56127613560fa436c
Nicolas Porter <porter.nicolas@gmail.com>
2022-07-03 00:00:33 -0700

    stow: move dotfile

---
+++ zsh/.zshrc.d/01-stow.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+
+export STOW_DIR="$DOTFILES"
+
+# This is where the list of dirs to stow are specified
+export STOWFILE="${STOWFILE:-$STOW_DIR/Stowfile}"
+
+# Remove macOS turds, they can cause issues
+# for f in **/.DS_STORE; do
+#     # shellcheck disable=SC2154
+#     echo "${fg[yellow]}removing turd:${fg[default]} $f";
+#     rm "$f"
+# done 2> /dev/null
+
+# Restow
+[[ ! -f "$STOWFILE" ]] && exit
+
+# Gitk just replaces the symlink with a file, we fix this here
+function fix_gitk() {
+    gitk="$HOME/.config/git/gitk"
+    if [ ! -L "$gitk" ]; then
+        mv "$gitk" "$DOTFILES/git/.config/git/gitk" || true
+    fi
+}
+
+function restow() {
+    setopt local_options no_monitor
+    while read -r package; do
+        [[ -z "$package" ]] && continue
+        [[ "$package" == git ]] && fix_gitk
+        # https://github.com/aspiers/stow/issues/65
+        (stow -R "$package" 2>&1 | grep -v "BUG in find_stowed_path") &
+    done <"$STOWFILE"
+    wait
+}
+
+restow
