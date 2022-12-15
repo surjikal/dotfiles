2b4347229735da17aeb82ad06ed9ba3c19083a73
Nicolas Porter <porter.nicolas@gmail.com>
2022-12-14 23:24:24 -0800

    zsh add lang

---
+++ zsh/.zshrc.d/00-lang.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# shellcheck shell=bash
+
+# pipenv / poetry / python told me to do this...
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
