bbe3b495af0c5d842d808e13393757f91332cfcb
Nicolas Porter <porter.nicolas@gmail.com>
2022-12-14 23:27:15 -0800

    gcloud: some fixes/tweaks

--- zsh/.zshrc.d/20-gcloud.sh
+++ zsh/.zshrc.d/20-gcloud.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env zsh
 # shellcheck shell=bash
 
+GCLOUD_HOME="${GCLOUD_HOME:-/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk}"
+[[ ! -d "$GCLOUD_HOME" ]] && return
 
-export GCLOUD_HOME=/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
-[[ ! -d $GCLOUD_HOME ]] && return
+CLOUDSDK_PYTHON_VERSION="3.8.12"
+export CLOUDSDK_PYTHON="${PYENV_ROOT}/versions/${CLOUDSDK_PYTHON_VERSION}/bin/python"
+export CLOUDSDK_GSUTIL_PYTHON="$CLOUDSDK_PYTHON"
+export GCLOUD_HOME
+
+[[ ! -f "$CLOUDSDK_PYTHON" ]] && echo "python not found for CLOUDSDK_PYTHON: $CLOUDSDK_PYTHON"
 
 # shellcheck source=/dev/null
 source "$GCLOUD_HOME/path.zsh.inc"
+
 # shellcheck source=/dev/null
 source "$GCLOUD_HOME/completion.zsh.inc"
-
-export CLOUDSDK_GSUTIL_PYTHON=${CLOUDSDK_GSUTIL_PYTHON:-$CLOUDSDK_PYTHON}
